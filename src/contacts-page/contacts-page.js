4651d294ce044d6faf025f7234d7970c031a6a25
nordwaldberg <nord.waldberg@gmail.com>
2021-01-22 18:07:58 +0300

    Creating the contacts-page component completed

--- src/contacts-page/contacts-page.js
+++ src/contacts-page/contacts-page.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import styles from './contacts-page.module.scss';
 
-const ContactsPage = () => {};
+const ContactsPage = () => {
+    return (
+        <div className={`${styles['contacts-page']}`}>
+                <div className={`${styles['contacts-page__list']}`}>
+                    <a href="https://github.com/nordwaldberg"
+                       target="_blank"
+                       rel="noreferrer noopener"
+                       className={styles["contacts-page__list__item"]}>.github</a>
+                    <a href="mailto:nord.waldberg@gmail.com"
+                       target="_blank"
+                       rel="noreferrer noopener"
+                       className={styles["contacts-page__list__item"]}>.email</a>
+                    <a href="https://t.me/nordwaldberg"
+                       target="_blank"
+                       rel="noreferrer noopener"
+                       className={styles["contacts-page__list__item"]}>.telegram</a>
+                    <a href="https://spb.hh.ru/resume/9adcb3c2ff088f64c30039ed1f5a79796f345a"
+                       target="_blank"
+                       rel="noreferrer noopener"
+                       className={styles["contacts-page__list__item"]}>.hh</a>
+                </div>
+        </div>
+    );
+};
 
 export default ContactsPage;
