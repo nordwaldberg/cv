3d18c870aa0aab8143001f64c056039a64cf78c9
nordwaldberg <nord.waldberg@gmail.com>
2021-03-05 17:15:18 +0300

    Refactoring main-page component

--- src/components/pages/main-page/main-page.js
+++ src/components/pages/main-page/main-page.js
@@ -11,17 +11,16 @@ const MainPage = () => {
                 <div className={styles["main-content__html-decor"]}>
                     {htmlDecor}
                 </div>
-                <div className={styles["main-content__greeting"]}>
+                <div className={styles["main-content__text"]}>
                     <Typing hideCursor="true"
-                            speed={55}>
+                            className={styles["main-content__text_greeting"]}
+                            speed={50}>
                         {greeting}
                     </Typing>
-                </div>
-                <div className={styles["main-content__description"]}>
                     <Typing hideCursor="true"
+                            className={styles["main-content__text_description"]}
                             speed={20}
-                            startDelay={2000}>
-
+                            startDelay={1800}>
                         {description}
                     </Typing>
                 </div>
